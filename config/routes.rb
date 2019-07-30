c42529fdfa7e685a8a05b7163ebb53a381e17508
Ancy <ancypaul9559@gmail.com>
2019-07-30 18:13:18 +0530

    Add notifications, navbar and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'static_pages#home'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :users
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
